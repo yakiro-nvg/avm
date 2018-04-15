a310ef62e69b71c4a5b937288dfd9ca0b77e70df
Giang Nv <innostory@users.noreply.github.com>
2018-04-15 23:21:04 +0700

    draft byte code format design

---
+++ src/private/chunk.h
@@ -0,0 +1,138 @@
+// Copyright (c) 2017-2018 Nguyen Viet Giang. All rights reserved.
+#ifndef _AVM_CHUNK_H_
+#define _AVM_CHUNK_H_
+
+#include <avm/prereq.h>
+
+/// NaN boxed constant.
+typedef union aconstant_u {
+    f64 f;
+    u64 u;
+} aconstant_t;
+
+/// Constant size is fixed 8 bytes.
+ASTATIC_ASSERT(sizeof(aconstant_t) == 8);
+
+/// Function import.
+typedef struct aimport_s {
+    u16 module;
+    u16 symbol;
+} aimport_t;
+
+// Import size is fixed 4 bytes.
+ASTATIC_ASSERT(sizeof(aimport_t) == 4);
+
+/** Byte code chunk.
+\brief Layout: [`header`] [`module prototypes`]
+AVM is dynamic, stack based abstract machine which rely on a efficient byte code
+representation called `code chunk`. That chunks is designed to be able to loads
+and executes directly by as little effort as possible. The code chunk includes
+everything AVM needs to execute a module, except external value (import). Code
+chunk is `portable` in theory. However, there's no need to do that in real-life.
+AVM will not even try to load an incompatible chunk. If we really **need** this
+feature, considering a separated rewriter to transforms it first then feeds the
+resulted compatible chunk to our poor AVM.
+
+\par Header format:
+\rst
+=====  ===================  ===================
+bytes  description          value
+=====  ===================  ===================
+4      signature            0x41 0x6E 0x79 0x00
+1      version number       0x{MAJOR}{MINOR}
+1      is big endian?       1 = big, 0 = little
+1      num of modules       unsigned byte
+1      is debug?            1 = yes, 0 = no
+=====  ===================  ===================
+\endrst
+*/
+typedef struct acode_chunk_s {
+    u8 signature[4];
+    u8 major_ver : 4;
+    u8 minor_ver : 4;
+    u8 big_endian;
+    u8 num_modules;
+    u8 debug;
+} acode_chunk_t;
+
+// Chunk header size is fixed 8 bytes.
+ASTATIC_ASSERT(sizeof(acode_chunk_t) == 8);
+
+/** Byte code prototype.
+\brief
+AVM byte code module, function and it's compiled data are represents by a simple
+recursive structure named `prototype`. Prototype is self-contained, portable in
+theory and relocatable. We can safely copy a memory blob of prototype, moving it
+around, serialization by just a simple fread/fwrite, and mmap as well.
+
+The root prototype must be a `module prototype`, there are no global function in
+AVM, all functions must be defined inside a module. The `module` prototype only
+consists of nested function and constant. That means the instruction, up-value,
+local variable and import will not be allowed to be defined at this level. That
+either don't make any sense or strictly prohibited to prevent the possibility of
+race conditions.
+
+Module level function may be exported by a symbolic name if `symbol to export`
+isn't negative. Otherwise this means a private function, which can only be used
+by other functions in the same module. The absolute value of symbol here is an
+index that points to the `string pool` (more later). AVM treats module symbols
+as `immutable` (however, can still be explicitly updated by loader). In other
+words we can't refer to these function as a variable, there are no variable in
+this situation as well. Therefore, to get reference to a private module function
+we also need to `import`s it the same way as public exported. That sounds weird
+at glance, but it makes our code chunk safe to shares in threaded environment.
+Besides that, the nested, or to be more precise anonymous closure, which bounds
+to a variable is private to lexical scope, and apparently private to the current
+VM instance. We can't share that variable no anyone from the outside, so there
+is no race condition to worry about. But all of that means it isn't exportable
+and must have a zero symbol.
+
+Small integer may be encoded directly in the `AOC_LSI` instruction, but for more
+complex, non trivial data likes constant, string and import we need a dedicated
+corresponding pool for each type. That must be reffered using index as well. The
+reason behind this decision is simple, we don't want to be messed with variable
+sized instruction and unaligned access. Constant is IEEE 754 double that must be
+8 bytes aligned. Constant string is boxed by NaN, with the payload is an index
+that points to the string pool.
+
+Debug information must be stripped if \ref acode_chunk_t `.debug` is `no`.
+
+\par Memory layout:
+\rst
+===================================================  ====================
+bytes                                                description
+===================================================  ====================
+sizeof(s16)                                          symbol to export
+sizeof(u16)                                          source file name
+sizeof(u16)                                          num string bytes
+sizeof(u16)                                          num local variables
+sizeof(u16)                                          num instructions
+sizeof(u16)                                          num nesteds
+sizeof(u16)                                          num constants
+sizeof(u16)                                          num imports
+num string bytes                                     strings
+num instructions * sizeof(:c:type:`ainstruction_t`)  instructions
+num constants * sizeof(:c:type:`aconstant_t`)        constants
+num nesteds * sizeof(:c:type:`aimport_t`)            imports
+num instructions * sizeof(u16)                       debug source lines
+num instructions * sizeof(u16)                       debug source columns
+num local variables * sizeof(u16)                    debug variable names
+_                                                    nested prototypes
+===================================================  ====================
+\endrst
+*/
+typedef struct acode_proto_s {
+    s16 symbol;
+    u16 source;
+    u16 string_bytes;
+    u16 num_local_vars;
+    u16 num_instructions;
+    u16 num_nesteds;
+    u16 num_constants;
+    u16 num_imports;
+} acode_proto_t;
+
+// Prototype header size is fixed 16 bytes.
+ASTATIC_ASSERT(sizeof(acode_proto_t) == 16);
+
+#endif // !_AVM_CHUNK_H_
---
+++ src/private/opcode.h
@@ -0,0 +1,328 @@
+// Copyright (c) 2017-2018 Nguyen Viet Giang. All rights reserved.
+#ifndef _AVM_OPCODE_H_
+#define _AVM_OPCODE_H_
+
+#include <avm/prereq.h>
+
+/// Specify the operation to be performed by the instructions.
+typedef enum aopcode_e {
+    AOC_NOP = 0,
+    AOC_BRK = 1,
+    AOC_POP = 2,
+
+    AOC_LDK = 10,
+    AOC_NIL = 11,
+    AOC_LDB = 12,
+    AOC_LSI = 13,
+    AOC_LLV = 14,
+    AOC_SLV = 15,
+    AOC_IMP = 16,
+    AOC_CLS = 17,
+
+    AOC_JMP = 30,
+    AOC_JIN = 31,
+
+    AOC_IVK = 40,
+    AOC_RET = 41,
+
+    AOC_ADD = 60,
+    AOC_SUB = 61,
+    AOC_MUL = 62,
+    AOC_DIV = 63
+} aopcode_t;
+
+/** Base type.
+\rst
+======  =======
+8 bits  24 bits
+======  =======
+opcode  payload
+======  =======
+\endrst
+*/
+typedef struct ai_base_s {
+    s32 opcode : 8;
+    s32 _ : 24;
+} ai_base_t;
+
+/** No operation.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_NOP  _
+=======  =======
+\endrst
+*/
+typedef struct ai_nop_s {
+    s32 _;
+} ai_nop_t;
+
+/** Suspend the execution until being resumed by debugger.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_BRK  _
+=======  =======
+\endrst
+*/
+typedef struct ai_brk_s {
+    s32 _;
+} ai_brk_t;
+
+/** Pop `n` elements from the stack.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_POP  n
+=======  =======
+\endrst
+*/
+typedef struct ai_pop_s {
+    s32 _ : 8;
+    s32 n : 24;
+} ai_pop_t;
+
+/** Push a constant from const pool at `idx` onto the stack.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_LDK  idx
+=======  =======
+\endrst
+*/
+typedef struct ai_ldk_s {
+    s32 _ : 8;
+    s32 idx : 24;
+} ai_ldk_t;
+
+/** Push a nil value onto the stack.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_NIL  _
+=======  =======
+\endrst
+*/
+typedef struct ai_nil_s {
+    s32 _;
+} ai_nil_t;
+
+/** Push a bool value onto the stack.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_LDB  value
+=======  =======
+\endrst
+*/
+typedef struct ai_ldb_s {
+    s32 _ : 8;
+    s32 value : 24;
+} ai_ldb_t;
+
+/** Push a small integer value (24 bits) onto the stack.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_LSI  value
+=======  =======
+\endrst
+*/
+typedef struct ai_lsi_s {
+    s32 _ : 8;
+    s32 value : 24;
+} ai_lsi_t;
+
+/** Push a value at local `slot` onto the stack.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_LLV  slot
+=======  =======
+\endrst
+*/
+typedef struct ai_llv_s {
+    s32 _ : 8;
+    s32 slot : 24;
+} ai_llv_t;
+
+/** Pop a value from the stack and set it to the local `slot`.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_SLV  slot
+=======  =======
+\endrst
+*/
+typedef struct ai_slv_s {
+    s32 _ : 8;
+    s32 slot : 24;
+} ai_slv_t;
+
+/** Push a value from import pool at `idx` onto the stack.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_IMP  idx
+=======  =======
+\endrst
+*/
+typedef struct ai_imp_s {
+    s32 _ : 8;
+    s32 idx : 24;
+} ai_imp_t;
+
+/** Make a closure of prototype at `idx` and push it onto the stack.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_CLS  idx
+=======  =======
+\endrst
+*/
+typedef struct ai_cls_s {
+    s32 _ : 8;
+    s32 idx : 24;
+} ai_cls_t;
+
+/** Unconditional jump to relative signed `offset`.
+\rst
+=======  ============
+8 bits   24 bits
+=======  ============
+AOC_JMP  offset
+=======  ============
+\endrst
+*/
+typedef struct ai_jmp_s {
+    s32 _ : 8;
+    s32 offset : 24;
+} ai_jmp_t;
+
+/** Conditional jump to relative signed `offset`.
+\brief Pop a boolean value from the stack and jump if isn't true.
+\rst
+=======  ============
+8 bits   24 bits
+=======  ============
+AOC_JIN  offset
+=======  ============
+\endrst
+*/
+typedef struct ai_jin_s {
+    s32 _ : 8;
+    s32 offset : 24;
+} ai_jin_t;
+
+/** Function call.
+\brief Please refer \ref avm_call for the calling convention.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_IVK  nargs
+=======  =======
+\endrst
+*/
+typedef struct ai_ivk_s {
+    s32 _ : 8;
+    s32 nargs : 24;
+} ai_ivk_t;
+
+/** Returning from a function call.
+\brief Please refer \ref avm_call for the calling convention.
+\rst
+=======  =======
+8 bits   24 bits
+=======  =======
+AOC_RET  _
+=======  =======
+\endrst
+*/
+typedef struct ai_ret_s {
+    s32 _;
+} ai_ret_t;
+
+/** Add two numbers.
+\rst
+=======  ============
+8 bits   24 bits
+=======  ============
+AOC_ADD  _
+=======  ============
+\endrst
+*/
+typedef struct ai_add_s {
+    s32 _;
+} ai_add_t;
+
+/** Subtract two numbers, top is subtrahend.
+\rst
+=======  ============
+8 bits   24 bits
+=======  ============
+AOC_SUB  _
+=======  ============
+\endrst
+*/
+typedef struct ai_sub_s {
+    s32 _;
+} ai_sub_t;
+
+/** Multiply two numbers.
+\rst
+=======  ============
+8 bits   24 bits
+=======  ============
+AOC_MUL  _
+=======  ============
+\endrst
+*/
+typedef struct ai_mul_s {
+    s32 _;
+} ai_mul_t;
+
+/** Divide two numbers, top is denominator.
+\rst
+=======  ============
+8 bits   24 bits
+=======  ============
+AOC_DIV  _
+=======  ============
+\endrst
+*/
+typedef struct ai_div_s {
+    s32 _;
+} ai_div_t;
+
+/// Variant of instruction types.
+typedef union {
+    ai_base_t b;
+    ai_pop_t pop;
+    ai_ldk_t ldk;
+    ai_ldb_t ldb;
+    ai_lsi_t lsi;
+    ai_llv_t llv;
+    ai_slv_t slv;
+    ai_imp_t imp;
+    ai_cls_t cls;
+    ai_jmp_t jmp;
+    ai_jin_t jin;
+    ai_ivk_t ivk;
+} ainstruction_t;
+
+// Instruction size is fixed 4 bytes.
+ASTATIC_ASSERT(sizeof(ainstruction_t) == 4);
+
+#endif // !_AVM_OPCODE_H_
